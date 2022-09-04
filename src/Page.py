8d67f781abf331e46e355b2f12637dea89df8d38
FelixLuciano <felix.lucianodias@gmail.com>
2022-09-04 14:22:10 -0300

    refactor: :recycle: Split Page into Parser, Style and Props

--- src/Page.py
+++ src/Page.py
@@ -1,69 +1,17 @@
-from os.path import getmtime
-from pathlib import Path
-
 import __main__
-import cssutils
-from bs4 import BeautifulSoup
-from ipywidgets import Widget
-from jinja2 import BaseLoader, Environment, TemplateNotFound
 
-from .Workspace import Workspace
+from .Notebook import Workspace
 from .Parser import Parser
-from .Preview import Preview
-from .Send import Send
+from .Props import Props
 
 
-class Page:
-    user_props = {}
+class Page():
+    def __init__(self):
+        self.update_template()
 
-    @staticmethod
-    def user_prop(func):
-        Page.user_props[func.__name__] = func
+    def update_template(self):
+        source = Workspace.get_html_source()
+        self.template = Parser.parse(source)
 
-        return func
-
-    @staticmethod
-    def get_props(user):
-        props = {}
-
-        for name, value in vars(__main__).items():
-            if name != "users" and not name.startswith("_"):
-                if isinstance(value, Widget):
-                    props[name] = value.value
-                else:
-                    props[name] = value
-
-        props.update(user)
-        props.update({key: handler(user) for key, handler in Page.user_props.items()})
-
-        return props
-
-    @staticmethod
-    def get_template():
-        return Parser.parse(Workspace.get_html_source())
-
-    @staticmethod
-    def render(user):
-        return Page.get_template().render(**Page.get_props(user))
-
-    @staticmethod
-    def preview():
-        Preview.display(Page)
-
-    @staticmethod
-    def send():
-        return Send.display(Page)
-
-    class Loader(BaseLoader):
-        def get_source(self, environment, template):
-            filename = Path(Path.cwd(), template)
-
-            if not filename.exists():
-                raise TemplateNotFound(str(filename))
-
-            mtime = getmtime(filename)
-
-            with open(filename) as source_file:
-                source = source_file.read()
-
-            return source, filename, lambda: mtime == getmtime(filename)
+    def render(self, user):
+        return self.template.render(**Props.get_props(user))
