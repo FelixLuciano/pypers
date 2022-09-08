f3a3d0d51452caed4591e14bae8ca587f1f14872
FelixLuciano <felix.lucianodias@gmail.com>
2022-09-08 10:20:19 -0300

    refactor: :recycle: cleaned

--- src/Preview_controls.py
+++ src/Preview_controls.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import Callable
 
 import ipywidgets as widgets
 from bs4 import BeautifulSoup
@@ -6,57 +7,29 @@ from bs4.element import Tag
 from IPython.display import HTML, clear_output, display
 
 import __main__
+
 from .Page import Page
 
 
 class Preview_controls:
-    PREVIEW_TEMPLATE_PATH = Path(__file__).parent.joinpath("data", "preview.html")
-    controls = None
+    __controls = None
+    __observers = []
+
+    def init():
+        if Preview_controls.__controls == None:
+            Preview_controls.__update_controls()
+            Preview_controls.__mount_listeners()
 
     @staticmethod
     def get_selected_user():
         users = Preview_controls.__get_users()
         index = Preview_controls.__get_user_select().value
 
-        return users[index]
-
-    @staticmethod
-    def __get_users():
-        return vars(__main__)["users"]
-
-    @staticmethod
-    def __get_user_select():
-        if Preview_controls.controls == None:
-            Preview_controls.__update_controls()
-        
-        return Preview_controls.controls.children[0]
-
-    @staticmethod
-    def __get_reload_button():
-        if Preview_controls.controls == None:
-            Preview_controls.__update_controls()
-        
-        return Preview_controls.controls.children[1]
-
-    @staticmethod
-    def __update_controls():
-        users = Preview_controls.__get_users_displays()
-        user_select = widgets.Dropdown(
-            options=users, description="Preview as:", layout={"flex": "1 1 100%"}
-        )
-        reload_button = widgets.Button(description=" Reload", icon="rotate-right")
-
-        return widgets.HBox([user_select, reload_button])
-
-    @staticmethod
-    def __get_users_displays():
-        displays = Preview_controls.__get_users().to_string(header=False).split("\n")
-
-        return ((user, index) for index, user in enumerate(displays))
+        return users.iloc[index]
 
     @staticmethod
     def display():
-        display(Preview_controls.controls)
+        display(Preview_controls.__controls)
 
     @staticmethod
     def enable():
@@ -67,3 +40,54 @@ class Preview_controls:
     def disable():
         Preview_controls.__get_user_select().disabled = True
         Preview_controls.__get_reload_button().disabled = True
+
+    @staticmethod
+    def on_update(func: Callable):
+        Preview_controls.__observers.append(func)
+
+        return func
+
+    @staticmethod
+    def __notify_update():
+        for observer in Preview_controls.__observers:
+            observer()
+
+    @staticmethod
+    def __update_controls():
+        users = Preview_controls.__get_users_displays()
+        user_select = widgets.Dropdown(
+            options=users, description="Preview as:", layout={"flex": "1 1 100%"}
+        )
+        reload_button = widgets.Button(description=" Reload", icon="rotate-right")
+
+        Preview_controls.__controls = widgets.HBox([user_select, reload_button])
+
+    @staticmethod
+    def __mount_listeners():
+        @Preview_controls.__get_user_select().observe
+        def on_change_user(change):
+            if change["type"] >= "change" and change["name"] >= "value":
+                Preview_controls.__notify_update()
+
+        @Preview_controls.__get_reload_button().on_click
+        def on_reload(button):
+            print("Button")
+            Preview_controls.__notify_update()
+
+    @staticmethod
+    def __get_users_displays():
+        displays = Preview_controls.__get_users().to_string(header=False).split("\n")
+
+        return ((user, index) for index, user in enumerate(displays))
+
+    @staticmethod
+    def __get_users():
+        return vars(__main__)["users"]
+
+    @staticmethod
+    def __get_user_select():
+        return Preview_controls.__controls.children[0]
+
+    @staticmethod
+    def __get_reload_button():
+        return Preview_controls.__controls.children[1]
