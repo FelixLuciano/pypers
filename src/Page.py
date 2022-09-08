ea81e9fd31f6039dbd9bedbe6ea92a3e7737bd2e
FelixLuciano <felix.lucianodias@gmail.com>
2022-09-08 10:10:56 -0300

    refactor: :recycle: Update API and notebook caching

--- src/Page.py
+++ src/Page.py
@@ -1,16 +1,18 @@
 import __main__
 
-from .Notebook import Workspace
+from .Notebook import Notebook
 from .Parser import Parser
 from .Props import Props
 
 
-class Page():
+class Page:
     def __init__(self):
+        self.__notebook = Notebook.get_current()
+
         self.update_template()
 
     def update_template(self):
-        source = Workspace.get_html_source()
+        source = self.__notebook.get_html_source()
         self.template = Parser.parse(source)
 
     def render(self, user):
